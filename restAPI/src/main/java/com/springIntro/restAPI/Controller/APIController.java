df1046c5edcf0603584b090a7d5228bd20f2caef
dhananjayghagre <dhananjayghagre95@gmail.com>
2025-02-22 13:09:06 +0530

    [Dhananjay Ghagre] Adding UC3_RestCallAsPathVariable

--- restAPI/src/main/java/com/springIntro/restAPI/Controller/APIController.java
+++ restAPI/src/main/java/com/springIntro/restAPI/Controller/APIController.java
@@ -2,6 +2,7 @@ package com.springIntro.restAPI.Controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,9 @@ public class APIController {
         return "Hello "+mark+" from Bridgelabz";
     }
 
+    @GetMapping("/hello/param/{name}")
+    public String hello(@PathVariable String name) {
+        return "Hello "+name+" from Bridgelabz";
+    }
 
 }
